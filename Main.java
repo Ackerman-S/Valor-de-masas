585866e00723f26158af68c1263d807da7f16c18
Ackerman-S <73323676+Ackerman-S@users.noreply.github.com>
2020-11-20 20:03:39 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -9,7 +9,7 @@ public class Main {
         Scanner rd = new Scanner(System.in);
         double cant, resultado;
         String unidad;
-        System.out.println("INGRESE UNIDAD kg/lb");
+        System.out.println("INGRESE UNIDAD km/mi");
         unidad = rd.nextLine();
         System.out.println("INGRESE CANTIDAD");
         cant = rd.nextFloat();
@@ -18,22 +18,22 @@ public class Main {
 
         System.out.println(resultado);
 
-        }
+    }
 
     private static double conversion(String unid, double can) {
         double res=0;
 
-        if(unid.equals("kg")){
-            res=(double)(can*2.2);
+        if(unid.equals("km")){
+            res=(double)(can*0.62137);
 
         }
-     else if(unid.equals("lb")){
-            res=(double)(can/2.2);
+        else if(unid.equals("mi")){
+            res=(double)(can*1.60934);
 
         }
         return res;
     }
 
-}
 
+}
 
